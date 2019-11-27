9dc5f3ffc843711b4a03dd1ebba2b5d742fef937
Daniel Kol Adam <venomdrophearthstone@gmail.com>
2019-11-28 01:59:29 +0200

    current weather shows based on lat + long

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,13 @@
 City.destroy_all
 Guide.destroy_all
 
-bansko = City.create(name: 'bansko', internet: '3gb', weather: '25c', country: 'bulgaria', price: '1384',
+bansko = City.create(name: 'bansko', internet: '3gb', weather: nil, country: 'bulgaria', price: '1384', lat: '44.8404', long: '23.4857',
                      image: 'city_card_images/test.jpg')
 bansko.save!
 bansko_guide = Guide.create(title: 'bansko guide')
 bansko_guide.city = bansko
 bansko_guide.save!
-berlin = City.create(name: 'berlin', internet: '13gb', weather: '15c', country: 'germany', price: '1384',
+berlin = City.create(name: 'berlin', internet: '13gb', weather: nil, country: 'germany', price: '1384', lat: '41.8404', long: '23.4857',
                      image: 'city_card_images/test.jpg')
 berlin.save!
 berlin_guide = Guide.create(title: 'berlin guide')
@@ -20,43 +20,42 @@ berlin_guide.save!
 
 chiang_mai = City.create(name: 'chiang mai',
                          internet: '22gb',
-                         weather: '55c', country: 'thailand', price: '1384',
+                         weather: nil, country: 'thailand', price: '1384', lat: '41.8404', long: '23.4857',
                          image: 'city_card_images/test.jpg')
 chiang_mai.save!
 
-istanbul = City.create(name: 'istanbul', internet: '1gb', weather: '200c', country: 'turkey', price: '1384',
+istanbul = City.create(name: 'istanbul', internet: '1gb', weather: nil, country: 'turkey', price: '1384', lat: '41.8404', long: '23.4857',
                        image: 'city_card_images/test.jpg')
 istanbul.save!
 
-london = City.create(name: 'london', internet: '12gb', weather: '-5c', country: 'england', price: '2000',
+london = City.create(name: 'london', internet: '12gb', weather: nil, country: 'england', price: '2000', lat: '41.8404', long: '23.4857',
                      image: 'city_card_images/test.jpg')
 london.save!
 
-paris = City.create(name: 'paris', internet: '11gb', weather: '1c', country: 'france', price: '2000',
+paris = City.create(name: 'paris', internet: '11gb', weather: nil, country: 'france', price: '2000', lat: '41.8404', long: '23.4857',
                     image: 'city_card_images/test.jpg')
 paris.save!
 
-denver = City.create(name: 'denver', internet: '8gb', weather: '5c', country: 'USA', price: '2000',
+denver = City.create(name: 'denver', internet: '8gb', weather: nil, country: 'USA', price: '2000', lat: '41.8404', long: '23.4857',
                      image: 'city_card_images/test.jpg')
 denver.save!
 
-budapest = City.create(name: 'budapest', internet: '18gb', weather: '7c', country: 'hungary', price: '1384',
+budapest = City.create(name: 'budapest', internet: '18gb', weather: nil, country: 'hungary', price: '1384', lat: '41.8404', long: '23.4857',
                        image: 'city_card_images/test.jpg')
 budapest.save!
 
-prague = City.create(name: 'prague', internet: '22gb', weather: '-11c', country: 'czech republic', price: '1384',
+prague = City.create(name: 'prague', internet: '22gb', weather: nil, country: 'czech republic', price: '1384', lat: '41.8404', long: '23.4857',
                      image: 'city_card_images/test.jpg')
 prague.save!
 
-chicago = City.create(name: 'chicago', internet: '19gb', weather: '-33c', country: 'USA', price: '2000',
+chicago = City.create(name: 'chicago', internet: '19gb', weather: nil, country: 'USA', price: '2000', lat: '41.8404', long: '23.4857',
                       image: 'city_card_images/test.jpg')
 chicago.save!
 
-tokyo = City.create(name: 'tokyo', internet: '200gb', weather: '18c', country: 'japan', price: '2000',
+tokyo = City.create(name: 'tokyo', internet: '200gb', weather: nil, country: 'japan', price: '2000', lat: '41.8404', long: '23.4857',
                     image: 'city_card_images/test.jpg')
 tokyo.save!
 
-seoul = City.create(name: 'seoul', internet: '300gb', weather: '17c', country: 'korea', price: '2000',
+seoul = City.create(name: 'seoul', internet: '300gb', weather: nil, country: 'korea', price: '2000', lat: '41.8404', long: '23.4857',
                     image: 'city_card_images/test.jpg')
 seoul.save!
-City.update_all(lat: '41.8404', long: '23.4857')
